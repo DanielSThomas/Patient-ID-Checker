df87b41fb400d3e984cb79a3bf32a28980c8b972
AbroadGames <AbroadGames@users.noreply.github.com>
2019-12-09 19:07:55 +0000

    Update MainForm component names

--- DanielSThomasCodingExercise/DanielSThomasCodingExercise/MainForm.Designer.cs
+++ DanielSThomasCodingExercise/DanielSThomasCodingExercise/MainForm.Designer.cs
@@ -28,25 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnManualInput = new System.Windows.Forms.Button();
             this.lblManual = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtManualInput = new System.Windows.Forms.TextBox();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.lblOutput = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btnAutoInput = new System.Windows.Forms.Button();
+            this.lblManualOutput = new System.Windows.Forms.Label();
+            this.lblAuto = new System.Windows.Forms.Label();
+            this.lblAutoOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btnManualInput
             // 
-            this.button1.Location = new System.Drawing.Point(26, 121);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 36);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Validate ID";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.btnManualInput.Location = new System.Drawing.Point(26, 121);
+            this.btnManualInput.Name = "btnManualInput";
+            this.btnManualInput.Size = new System.Drawing.Size(131, 36);
+            this.btnManualInput.TabIndex = 0;
+            this.btnManualInput.Text = "Validate ID";
+            this.btnManualInput.UseVisualStyleBackColor = true;
+            this.btnManualInput.Click += new System.EventHandler(this.Button1_Click);
             // 
             // lblManual
             // 
@@ -58,13 +58,13 @@
             this.lblManual.TabIndex = 1;
             this.lblManual.Text = "Manual Validation";
             // 
-            // textBox1
+            // txtManualInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(26, 95);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(312, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.txtManualInput.Location = new System.Drawing.Point(26, 95);
+            this.txtManualInput.Name = "txtManualInput";
+            this.txtManualInput.Size = new System.Drawing.Size(312, 20);
+            this.txtManualInput.TabIndex = 2;
+            this.txtManualInput.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // lblTitle
             // 
@@ -76,62 +76,62 @@
             this.lblTitle.TabIndex = 3;
             this.lblTitle.Text = "Patient ID Checker";
             // 
-            // button2
+            // btnAutoInput
             // 
-            this.button2.Location = new System.Drawing.Point(26, 215);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 36);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Validate ID from file";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAutoInput.Location = new System.Drawing.Point(26, 215);
+            this.btnAutoInput.Name = "btnAutoInput";
+            this.btnAutoInput.Size = new System.Drawing.Size(131, 36);
+            this.btnAutoInput.TabIndex = 4;
+            this.btnAutoInput.Text = "Validate ID from file";
+            this.btnAutoInput.UseVisualStyleBackColor = true;
             // 
-            // lblOutput
+            // lblManualOutput
             // 
-            this.lblOutput.AutoSize = true;
-            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOutput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.lblOutput.Location = new System.Drawing.Point(271, 128);
-            this.lblOutput.Name = "lblOutput";
-            this.lblOutput.Size = new System.Drawing.Size(67, 29);
-            this.lblOutput.TabIndex = 5;
-            this.lblOutput.Text = "Valid";
+            this.lblManualOutput.AutoSize = true;
+            this.lblManualOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblManualOutput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblManualOutput.Location = new System.Drawing.Point(271, 128);
+            this.lblManualOutput.Name = "lblManualOutput";
+            this.lblManualOutput.Size = new System.Drawing.Size(67, 29);
+            this.lblManualOutput.TabIndex = 5;
+            this.lblManualOutput.Text = "Valid";
             // 
-            // label1
+            // lblAuto
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(105, 181);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 20);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Automatic Validation";
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            this.lblAuto.AutoSize = true;
+            this.lblAuto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAuto.Location = new System.Drawing.Point(105, 181);
+            this.lblAuto.Name = "lblAuto";
+            this.lblAuto.Size = new System.Drawing.Size(155, 20);
+            this.lblAuto.TabIndex = 6;
+            this.lblAuto.Text = "Automatic Validation";
+            this.lblAuto.Click += new System.EventHandler(this.Label1_Click);
             // 
-            // label2
+            // lblAutoOutput
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(191, 222);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 29);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "1/5 Valid IDs";
-            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            this.lblAutoOutput.AutoSize = true;
+            this.lblAutoOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAutoOutput.ForeColor = System.Drawing.Color.Red;
+            this.lblAutoOutput.Location = new System.Drawing.Point(191, 222);
+            this.lblAutoOutput.Name = "lblAutoOutput";
+            this.lblAutoOutput.Size = new System.Drawing.Size(147, 29);
+            this.lblAutoOutput.TabIndex = 7;
+            this.lblAutoOutput.Text = "1/5 Valid IDs";
+            this.lblAutoOutput.Click += new System.EventHandler(this.Label2_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(365, 279);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lblOutput);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblAutoOutput);
+            this.Controls.Add(this.lblAuto);
+            this.Controls.Add(this.lblManualOutput);
+            this.Controls.Add(this.btnAutoInput);
             this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtManualInput);
             this.Controls.Add(this.lblManual);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnManualInput);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -141,14 +141,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnManualInput;
         private System.Windows.Forms.Label lblManual;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtManualInput;
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label lblOutput;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAutoInput;
+        private System.Windows.Forms.Label lblManualOutput;
+        private System.Windows.Forms.Label lblAuto;
+        private System.Windows.Forms.Label lblAutoOutput;
     }
 }
 
